7bac2fbd1c3a1d8a6f99bebc3a7a3509a4440d9c
Mudit Lodha <muditlodha2000@gmail.com>
2024-01-30 10:29:11 +0530

    remove api key

--- chatgpt.py
+++ chatgpt.py
@@ -5,7 +5,7 @@ from langchain.indexes import VectorstoreIndexCreator
 from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
 
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = ""
 
 query = sys.argv[1]
 
