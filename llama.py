47da344041224450c280d4acce54ebb9e3118a79
Mudit Lodha <muditlodha2000@gmail.com>
2024-02-07 11:51:48 +0530

    add llama model prompt

---
+++ llama.py
@@ -0,0 +1,24 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import transformers
+import torch
+
+model_id = "codellama/CodeLlama-7b-hf"
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    torch_dtype=torch.float16
+).to("cuda")
+
+prefix = 'def remove_non_ascii(s: str) -> str:\n    """ '
+suffix = "\n    return result\n"
+
+prompt = f"<PRE> {prefix}<SUF>{suffix} <MID>"
+inputs = tokenizer(prompt, return_tensors="pt").to("cuda")
+
+output = model.generate(
+    inputs["input_ids"],
+    max_new_tokens=200,
+    do_sample=False,
+)
+output = output[0].to("cpu")
+print(tokenizer.decode(output))
