bef68bf35ffd4821af2bfb43c070a27b41615ed3
Mudit Lodha <muditlodha2000@gmail.com>
2024-02-12 14:04:27 +0530

    add quantization

--- deepseek.py
+++ deepseek.py
@@ -1,8 +1,15 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 import torch
 from datetime import datetime
 
-tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/deepseek-coder-7b-instruct", trust_remote_code=True)
+bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.bfloat16) # CUDA
+
+
+tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/deepseek-coder-7b-instruct", trust_remote_code=True, quantization_config=bnb_config)
 model = AutoModelForCausalLM.from_pretrained(
     "deepseek-ai/deepseek-coder-7b-instruct", 
     trust_remote_code=True, 
